020fece924769f857a2a63fdaf9ace5d8e80bc80
JuJu222 <64346619+JuJu222@users.noreply.github.com>
2024-06-18 13:14:42 +0700

    Added subscription type seeder

---
+++ src/prisma/seeders/subscriptionType.ts
@@ -0,0 +1,10 @@
+import { PrismaClient } from "@prisma/client"
+
+export async function seedSubscriptionTypes(prisma: PrismaClient) {
+    await prisma.accountType.create({
+        data: {
+            id: "4ff2b533-f5c9-4a9f-a1a9-a43b61bb7deb",
+            name: "Premium"
+        },
+    })
+}
