564a073aeb06d920d59cfc570d6662e2a3945504
JuJu222 <japjustin@yahoo.com>
2024-06-19 14:53:54 +0700

    Added date filter for transactions

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -35,10 +35,64 @@ router.get('/types', async (req, res) => {
 
 router.get('/:userId', async (req, res) => {
     const userId = req.params.userId;
+    const grouped = req.query.grouped;
+    const startDateStr = req.query.startDate;
+    const endDateStr = req.query.endDate;
 
     try {
-        if (req.query.grouped == "daily") {
-            const groupedTransactions: any = await prisma.$queryRaw`
+        if (grouped == "daily") {
+            if (startDateStr && startDateStr != "" && endDateStr && endDateStr != "") {
+                const groupedTransactions: any = await prisma.$queryRaw`
+            SELECT DATE_TRUNC('day', t.date) AS "transactionDate",
+                    SUM(t.amount) AS "totalAmount",
+                    array_agg(json_build_object(
+                        'id', t.id,
+                        'description', t.description,
+                        'amount', t.amount,
+                        'createdAt', t.created_at,
+                        'updatedAt', t.updated_at,
+                        'transactionCategory', json_build_object(
+                            'id', tc.id,
+                            'name', tc.name,
+                            'typeId', tc.type_id,
+                            'createdAt', tc.created_at,
+                            'updatedAt', tc.updated_at,
+                            'type', json_build_object(
+                                'id', tt.id,
+                                'name', tt.name,
+                                'createdAt', tt.created_at,
+                                'updatedAt', tt.updated_at
+                            )
+                        ),
+                        'financialAccount', json_build_object(
+                            'id', fa.id,
+                            'name', fa.name,
+                            'userId', fa.user_id,
+                            'categoryId', fa.category_id,
+                            'initialValue', fa.initial_value,
+                            'currentValue', fa.current_value,
+                            'createdAt', fa.created_at,
+                            'updatedAt', fa.updated_at
+                        )
+                    ) ORDER BY t.created_at DESC) AS transactions
+                FROM transactions t
+                JOIN transaction_categories tc ON t.category_id = tc.id
+                JOIN transaction_types tt ON tc.type_id = tt.id
+                JOIN financial_accounts fa ON t.account_id = fa.id
+                WHERE t.date >= ${startDateStr}::date AND t.date < ${endDateStr}::date
+                GROUP BY "transactionDate"
+                ORDER BY "transactionDate" DESC;
+        `;
+
+                const json = JSON.parse(JSON.stringify(groupedTransactions, (_, value) =>
+                    typeof value === 'bigint'
+                        ? value.toString()
+                        : value
+                ));
+
+                return res.status(200).json(json);
+            } else {
+                const groupedTransactions: any = await prisma.$queryRaw`
             SELECT DATE_TRUNC('day', t.date) AS "transactionDate",
                     SUM(t.amount) AS "totalAmount",
                     array_agg(json_build_object(
@@ -78,14 +132,15 @@ router.get('/:userId', async (req, res) => {
                 GROUP BY "transactionDate"
                 ORDER BY "transactionDate" DESC;
         `;
-            console.log(groupedTransactions);
 
-            const json = JSON.parse(JSON.stringify(groupedTransactions, (_, value) =>
-                typeof value === 'bigint'
-                    ? value.toString()
-                    : value
-            ));
-            return res.status(200).json(json);
+                const json = JSON.parse(JSON.stringify(groupedTransactions, (_, value) =>
+                    typeof value === 'bigint'
+                        ? value.toString()
+                        : value
+                ));
+
+                return res.status(200).json(json);
+            }
         } else {
             const transactions = await prisma.transaction.findMany({
                 where: {
