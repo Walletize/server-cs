0b358cf92d082917e1f61497cff26954a3a54624
JuJu222 <64346619+JuJu222@users.noreply.github.com>
2024-07-06 15:18:10 +0700

    Added transaction transfer tables + update and transfer categories

--- src/prisma/seeders/transactionCategory.ts
+++ src/prisma/seeders/transactionCategory.ts
@@ -3,17 +3,29 @@ import { PrismaClient } from "@prisma/client"
 export async function seedTransactionCategories(prisma: PrismaClient) {
     await prisma.transactionCategory.create({
         data: {
-            id: "f22939a2-916e-4353-aa2d-51a400a007f3",
-            name: "Salary",
-            typeId: "a6f2747a-8d68-49f7-9aab-3a9dcaaee850",
+            name: "Gain",
+            typeId: "62919f5b-047d-45c7-96d9-1cd21a946d3a",
         },
-    })
+    });
 
     await prisma.transactionCategory.create({
         data: {
-            id: "94c3f747-d94b-426b-9363-d4c7f0243a95",
-            name: "Entertainment",
+            name: "Loss",
             typeId: "62919f5b-047d-45c7-96d9-1cd21a946d3a",
         },
-    })
+    });
+
+    await prisma.transactionCategory.create({
+        data: {
+            name: "In Walletize",
+            typeId: "62919f5b-047d-45c7-96d9-1cd21a946d3a",
+        },
+    });
+
+    await prisma.transactionCategory.create({
+        data: {
+            name: "Out of Walletize",
+            typeId: "62919f5b-047d-45c7-96d9-1cd21a946d3a",
+        },
+    });
 }
