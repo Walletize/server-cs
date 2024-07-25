90d4b47f705e2813e5472e3a5e9f30d99da9baf2
JuJu222 <japjustin@yahoo.com>
2024-07-25 12:39:34 +0700

    Added change subscription plan endpoint

--- src/routes/subscriptions.ts
+++ src/routes/subscriptions.ts
@@ -9,7 +9,7 @@ router.post('/cancel/:subscriptionId', async (req, res) => {
             return res.status(500).json({ message: "Subscription id not found" });
         };
 
-        const cancel = await fetch(`https://sandbox-api.paddle.com/subscriptions/${subscriptionId}/cancel`, {
+        const cancelRes = await fetch(`https://sandbox-api.paddle.com/subscriptions/${subscriptionId}/cancel`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -19,10 +19,8 @@ router.post('/cancel/:subscriptionId', async (req, res) => {
                 effective_from: "next_billing_period"
             })
         });
-        const body = await cancel.json()
-        console.log(body)
 
-        if (cancel.ok) {
+        if (cancelRes.ok) {
             return res.status(200).json({ message: "Cancel subscription succesful" });
         } else {
             return res.status(500).json({ message: "Cancel subscription failed" });
@@ -30,6 +28,36 @@ router.post('/cancel/:subscriptionId', async (req, res) => {
     } catch (e) {
         console.log(e)
         return res.status(500).json({ message: "Cancel subscription failed" });
+    }
+});
+
+router.patch('/change/:subscriptionId', async (req, res) => {
+    try {
+        const subscriptionId = req.params.subscriptionId;
+        if (!subscriptionId) {
+            return res.status(500).json({ message: "Subscription id not found" });
+        };
+        const items = req.body;
+
+        const changeRes = await fetch(`https://sandbox-api.paddle.com/subscriptions/${subscriptionId}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + process.env.PADDLE_API_KEY!,
+            },
+            body: JSON.stringify({
+                items
+            })
+        });
+
+        if (changeRes.ok) {
+            return res.status(200).json({ message: "Change subscription succesful" });
+        } else {
+            return res.status(500).json({ message: "Change subscription failed" });
+        }
+    } catch (e) {
+        console.log(e)
+        return res.status(500).json({ message: "Change subscription failed" });
     }
 });
 
