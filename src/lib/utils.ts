7172cc934d94bd72fae81f51de94d3f2187e3221
JuJu222 <64346619+JuJu222@users.noreply.github.com>
2024-08-14 19:10:02 +0700

    Added previous values to get transactions by user id

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -25,3 +25,63 @@ export async function updateCurrencyRates() {
         console.error('Error in scheduled task:', error);
     }
 }
+
+export function getStartDateOfCurrentMonth(): string {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = (now.getMonth() + 1).toString().padStart(2, '0');
+    const day = '01'; // The first day of the month
+
+    return `${year}-${month}-${day}`;
+};
+
+export function getPreviousPeriod(startDateStr: string, endDateStr: string): { startDate: string, endDate: string } {
+    const startDate = new Date(startDateStr);
+    const endDate = new Date(endDateStr);
+
+    const periodDuration = endDate.getTime() - startDate.getTime();
+
+    const prevEndDate = new Date(startDate.getTime() - 1); // One day before the original start date
+    const prevStartDate = new Date(prevEndDate.getTime() - periodDuration);
+
+    const formatDate = (date: Date): string => {
+        const year = date.getFullYear();
+        const month = (date.getMonth() + 1).toString().padStart(2, '0');
+        const day = date.getDate().toString().padStart(2, '0');
+        return `${year}-${month}-${day}`;
+    };
+
+    return {
+        startDate: formatDate(prevStartDate),
+        endDate: formatDate(prevEndDate),
+    };
+}
+
+export function getPreviousMonthPeriod() {
+    const now = new Date();
+    let year = now.getFullYear();
+    let month = now.getMonth();
+
+    if (month === 0) {
+        month = 11;
+        year--;
+    } else {
+        month--;
+    }
+
+    const startDate = new Date(year, month, 1);
+
+    const endDate = new Date(year, month + 1, 0); 
+
+    const formatDate = (date: Date): string => {
+        const year = date.getFullYear();
+        const month = (date.getMonth() + 1).toString().padStart(2, '0');
+        const day = date.getDate().toString().padStart(2, '0');
+        return `${year}-${month}-${day}`;
+    };
+
+    return {
+        startDate: formatDate(startDate),
+        endDate: formatDate(endDate),
+    };
+}
